33ba76a39f7f6c3f1e829a186af5943d02235ed9
Pablo Charamello <108502243+PabloCharamello@users.noreply.github.com>
2022-08-13 02:43:08 -0300

    view login casi terminada sin modal

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ app.set("view engine", "ejs");
 
 routes(app);
 
-
 app.listen(APP_PORT, () => {
   console.log(`\n[Express] Servidor corriendo en el puerto ${APP_PORT}.`);
   console.log(`[Express] Ingresar a http://localhost:${APP_PORT}.\n`);
