796061cd835e0a942efcc6addc559d8439a6193d
sdehl <sjdehlr@gmail.com>
2022-08-13 23:08:11 -0300

    profile and routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const routes = express.Router();
+const Tweet = require("../models/Tweet");
 const userController = require("../controllers/userController");
 const tweetController = require("../controllers/tweetController");
+const User = require("../models/User");
 
-// userRou.tes.
+// userRoutes.
 routes.get("/login", userController.login);
 
 routes.get("/register", userController.create);
@@ -12,5 +14,23 @@ routes.post("/register", userController.store);
 // tweetsRoutes
 routes.get("/", tweetController.index);
 routes.post("/", tweetController.store);
+
+routes.post("/like", async (req, res) => {
+  console.log(req.body.like);
+  //   const newLike = await new Tweet({
+  //     like: req.body.like,
+  //   });
+  res.redirect("/");
+});
+
+routes.get("/profile/:id", async (req, res) => {
+  const userTweets = await User.findById(req.params.id).populate("tweets");
+
+  console.log(userTweets);
+  //   const userTweets = await Tweet.find()
+  //     .sort([["createdAt", "descending"]])
+  //     .populate("user");
+  res.render("profile", { userTweets });
+});
 
 module.exports = routes;
